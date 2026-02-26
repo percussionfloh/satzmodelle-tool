9fc35c1f9bd7b8f1f96848b0b321dbcac35f8117
percussionfloh <florian.moldaschl@web.de>
2026-02-26 22:24:49 +0100

    update satzmodlle.vue

--- app/pages/satzmodelle.vue
+++ app/pages/satzmodelle.vue
@@ -9,20 +9,180 @@
                 <div>
                     <h2>French augmented Sixth</h2>
                     <VerovioCanvas view-mode="horizontal" url="/krn/french.krn" :scale="35" :page-margin="20" />
-                    <MidiPlayer url="/krn/french.krn" class="text-2xl"/>
+                    <!-- <MidiPlayer url="/krn/french.krn" class="text-2xl"/> -->
                 </div>
                 <div>
-                <h2>German augmented Sixth</h2>
-                <VerovioCanvas view-mode="horizontal" url="/krn/german.krn" :scale="35" :page-margin="20" />
+                    <h2>German augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/german.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/german.krn" class="text-2xl"/> -->
                 </div>
                 <div>
                     <h2>Italian augmented Sixth</h2>
                     <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
-                    <MidiPlayer url="/krn/italian.krn" class="text-2xl"/>
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
                 </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Italian augmented Sixth</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                
 
-                <VerovioCanvas view-mode="horizontal" url="/krn/fonte-5-1.krn" :scale="35" :page-margin="20" />
-                <VerovioCanvas view-mode="horizontal" url="/krn/fonte-7-1.krn" :scale="35" :page-margin="20" />
         </div>
     </UContainer>
 </template>

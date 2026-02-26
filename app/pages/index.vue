7e912efbc8cd13b1e73413055f6f53edeabfb7f8
percussionfloh <florian.moldaschl@web.de>
2026-02-26 23:20:30 +0100

    update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,21 @@
-<template>      
-  <div>
-  </div>
+<script setup>
+const { data } = await useAsyncData('satzmodelle', () => queryCollection('satzmodelle').all())
+const localePath = useLocalePath();
+
+ const { t } = useI18n();
+</script>
+
+<template>
+    <UCard v-for="piece in data">
+        <div class="flex flex-col gap-8">
+            <h1 class="text-3xl font-bold">Satzmodelle</h1>
+            <h2>French augmented Sixth</h2>
+        <div class="ml-auto">
+            <NuxtLink :to="localePath('/satzmodelle')" class="flex flex-col gap-4">
+                    {{ piece.title }}
+                    <VerovioCanvas  view-mode="horizontal" :url="piece.scorePath" :scale="35" :page-margin="20" />
+            </NuxtLink>
+        </div>
+        </div>
+    </UCard>
 </template>

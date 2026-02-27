871ca5114e1f041df23ef09687cb3ea08a9f7e1f
percussionfloh <florian.moldaschl@web.de>
2026-02-27 21:54:58 +0100

    update augmented6.vue and fauxbourdon.vue

--- app/pages/augmented6.vue
+++ app/pages/augmented6.vue
@@ -1,1 +1,26 @@
-<template></template>
+<script setup>
+ const { t } = useI18n();
+</script>
+
+<template>
+    <UContainer>
+        <h1 class="text-3xl font-bold">Übermäßige Sextakkorde</h1>
+            <div class="flex flex-col gap-8">
+                <div>
+                    <h2>Italienischer Übermäßiger Sextakkord</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/italian.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/italian.krn" class="text-2xl"/> -->
+                </div>
+                <div>
+                    <h2>Französischer Übermäßiger Sextakkord</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/french.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/french.krn" class="text-2xl"/> -->
+                </div>
+                <div class="flex flex-col gap-8">
+                    <h2>Deutscher Übermäßiger Sextakkord</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/german.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/german.krn" class="text-2xl"/> -->
+                </div>
+            </div>
+    </UContainer>
+</template>
--- app/pages/fauxbourdon.vue
+++ app/pages/fauxbourdon.vue
@@ -1,1 +1,36 @@
-<template></template>
+<script setup>
+ const { t } = useI18n();
+</script>
+
+<template>
+    <UContainer>
+        <h1 class="text-3xl font-bold">Fauxbourdon</h1>
+            <div class="flex flex-col gap-8">
+                    <div>
+                    <h2>Fauxbourdon aufwärts, 5-6-Seitenbewegung</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/fobo-5-6seitenbewegung.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/fobo-5-6seitenbewegung.krn" class="text-2xl"/> -->
+                </div>
+                <div class="flex flex-col gap-8">
+                    <h2>Fauxbourdon abwärts, 7-6-Konsekutive</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/fobo-7-6consecutive-abwaerts.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/fobo-7-6consecutive-abwaerts.krn" class="text-2xl"/> -->
+                </div>
+                <div class="flex flex-col gap-8">
+                    <h2>Fauxbourdon aufwärts, 7-6-Konsekutive</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/fobo-7-6consecutive-aufwaerts.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/fobo-7-6consecutive-aufwaerts.krn" class="text-2xl"/> -->
+                </div>
+                <div class="flex flex-col gap-8">
+                    <h2>Fauxbourdon abwärts</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/fobo-abwaerts.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/fobo-abwaerts.krn" class="text-2xl"/> -->
+                </div>
+                <div class="flex flex-col gap-8">
+                    <h2>Fauxbourdon aufwärts</h2>
+                    <VerovioCanvas view-mode="horizontal" url="/krn/fobo-aufwaerts.krn" :scale="35" :page-margin="20" />
+                    <!-- <MidiPlayer url="/krn/fobo-aufwaerts.krn" class="text-2xl"/> -->
+                </div>
+            </div>
+    </UContainer>
+</template>

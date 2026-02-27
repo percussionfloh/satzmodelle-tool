da65a4276459907ec4c7a683638f78a6ba5247f1
percussionfloh <florian.moldaschl@web.de>
2026-02-27 21:38:53 +0100

    update monteFonte.vue

--- app/pages/fonteMonte.vue
+++ app/pages/fonteMonte.vue
@@ -1,1 +1,32 @@
-<template></template>
+<script setup>
+ const { t } = useI18n();
+</script>
+<template>    
+    <UContainer>
+        <div class="flex flex-col gap-8">
+            <h1 class="text-xl font-bold">Fonte</h1>
+            <div>
+                <h2>Fonte mit Bassklauseln</h2>
+                <VerovioCanvas view-mode="horizontal" url="/krn/fonte-5-1.krn" :scale="35" :page-margin="20" />
+                <!-- <MidiPlayer url="/krn/fonte-5-1.krn" class="text-2xl"/> -->
+            </div>
+            <div>
+                <h2>Fonte mit Sopranklauseln</h2>
+                <VerovioCanvas view-mode="horizontal" url="/krn/fonte-7-1.krn" :scale="35" :page-margin="20" />
+                <!-- <MidiPlayer url="/krn/fonte-7-1.krn" class="text-2xl"/> -->
+            </div>
+            <h1 class="text-xl font-bold">Monte</h1>
+            <div>
+                <h2>Monte mit Bassklauseln</h2>
+                <VerovioCanvas view-mode="horizontal" url="/krn/monte-5-1.krn" :scale="35" :page-margin="20" />
+                <!-- <MidiPlayer url="/krn/monte-5-1.krn" class="text-2xl"/> -->
+            </div>
+            <div>
+                <h2>Monte mit Sopranklauseln</h2>
+                <VerovioCanvas view-mode="horizontal" url="/krn/monte-7-1.krn" :scale="35" :page-margin="20" />
+                <!-- <MidiPlayer url="/krn/monte-7-1.krn" class="text-2xl"/> -->
+            </div>
+        </div>
+    </UContainer>  
+</template>
+

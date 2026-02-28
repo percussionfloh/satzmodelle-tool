cd49cd397b385cd45ea35514449fb77379cb1a1e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2026-02-28 19:44:37 +0100

    Add currentCommitSha as runtime config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,24 @@
+import { execSync } from 'node:child_process';
+
+function getCurrentCommitSha() {
+    try {
+        return execSync(`git rev-parse --short=7 HEAD`, { encoding: 'utf8' }).toString().trim();
+    } catch {
+        return '';
+    }
+}
+
+const currentCommitSha = getCurrentCommitSha();
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: false },
+    runtimeConfig: {
+        public: {
+            currentCommitSha,
+        },
+    },
     modules: ['@nuxt/content', '@nuxt/ui',  '@pinia/nuxt','@nuxtjs/i18n'],
     css: ['~/assets/main.css'],
     i18n: {

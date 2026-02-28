092b3e61a900cdfb2469f8c25388312b3e4674a4
percussionfloh <florian.moldaschl@web.de>
2026-02-28 16:08:13 +0100

    update fonteMonte.vue

--- app/pages/fonteMonte.vue
+++ app/pages/fonteMonte.vue
@@ -4,7 +4,10 @@
 <template>    
     <UContainer>
         <div class="flex flex-col gap-8">
-            <h1 class="text-xl font-bold">Fonte</h1>
+            <div>
+                <h1 class="text-xl font-bold">Fonte</h1>
+                <UButton target="_blank" to="https://www.youtube.com/watch?v=r6Llu_uGY2U">Unter Fonte versteht Riepel eine VI-ii-V-I-Quintfallsequenz </UButton>
+            </div>
             <div>
                 <h2>Fonte mit Bassklauseln</h2>
                 <VerovioCanvas view-mode="horizontal" url="/krn/fonte-5-1.krn" :scale="35" :page-margin="20" />
@@ -15,7 +18,10 @@
                 <VerovioCanvas view-mode="horizontal" url="/krn/fonte-7-1.krn" :scale="35" :page-margin="20" />
                 <!-- <MidiPlayer url="/krn/fonte-7-1.krn" class="text-2xl"/> -->
             </div>
-            <h1 class="text-xl font-bold">Monte</h1>
+            <div>
+                <h1 class="text-xl font-bold">Monte</h1>
+                <UButton target="_blank" to="https://www.youtube.com/watch?v=1kZx35yG9UU">(italienisch monte ‚Berg‘) bezeichnet in der Musiktheorie eine zweigliedrige Sequenz, deren erstes Glied in der Tonart der IV. Stufe einer Dur- oder Molltonart schließt und deren zweites Glied eine Sekunde höher mit einem Halbschluss in dieser Haupttonart endet. Insbesondere in der europäischen Musik des 18. Jahrhunderts ist dieses Satzmodell von Bedeutung.[1]</UButton>
+            </div>
             <div>
                 <h2>Monte mit Bassklauseln</h2>
                 <VerovioCanvas view-mode="horizontal" url="/krn/monte-5-1.krn" :scale="35" :page-margin="20" />

e2b0cf5cd0ddb3108e7c05049c61fa563aa99660
percussionfloh <florian.moldaschl@web.de>
2026-03-03 20:21:29 +0100

    update app.config.ts

--- app/workers/app.config.ts
+++ app/workers/app.config.ts
@@ -5,5 +5,17 @@ export default defineAppConfig({
       secondary: 'fuchsia',
       neutral: 'zinc'
     }
-  }
+  },
+    prose: {
+      accordion: {
+        slots: {
+          root: 'my-5',
+          trigger: 'text-base'
+        }
+      },
+      accordionItem: {
+        base: 'pb-4 text-muted *:first:mt-0 *:last:mb-0 *:my-1.5'
+      }
+    }
 })
+

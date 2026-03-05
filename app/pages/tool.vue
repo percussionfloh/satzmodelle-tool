1dbd4d0d4882f00109bade9977fe14e5d40290d5
percussionfloh <florian.moldaschl@web.de>
2026-03-05 17:45:16 +0100

    update tool: Gewichtung tags

--- app/pages/tool.vue
+++ app/pages/tool.vue
@@ -6,6 +6,11 @@ const { data: satzmodelle } = await useAsyncData(`satzmodelle/*`, () => queryCol
 const localePath = useLocalePath();
 
 const tags = [...new Set(satzmodelle.value.flatMap(item => item.tags ?? []))]
+
+const stageOneTags = computed(() => tags.filter(tag => tag.toLowerCase().includes('wichtig')).sort((a, b) => t(a).localeCompare(t(b))));
+const stageTwoTags = computed(() => tags.filter(tag => tag.toLowerCase().includes('mittel')).sort((a, b) => t(a).localeCompare(t(b))));
+const stageThreeTags = computed(() => tags.filter(tag => tag.toLowerCase().includes('wenig')).sort((a, b) => t(a).localeCompare(t(b))));
+
 const selectedTags = ref([]);
 
 function toggleTag(tag) {
@@ -16,14 +21,21 @@ function toggleTag(tag) {
     }
 }
 
-
+console.log(tags);
 
 </script>
 
+
 <template class="flex flex-wrap gap-6">
-    <div class="max-w-7xl mx-auto">
+    <div class="flex flex-col items-center gap-8 mt-8 px-20">
         <div class="flex flex-wrap gap-6">
-            <UButton v-for="tag in tags" :color="selectedTags.includes(tag) ? 'primary' : 'neutral'" @click="toggleTag(tag)" :label="$t(tag)" />
+            <UButton v-for="tag in stageOneTags" :color="selectedTags.includes(tag) ? 'primary' : 'neutral'" @click="toggleTag(tag)" :label="$t(tag)" />
+        </div>
+        <div class="flex flex-wrap gap-6">
+            <UButton v-for="tag in stageTwoTags" :color="selectedTags.includes(tag) ? 'primary' : 'neutral'" @click="toggleTag(tag)" :label="$t(tag)" />
+        </div> 
+        <div class="flex flex-wrap gap-6">
+            <UButton v-for="tag in stageThreeTags" :color="selectedTags.includes(tag) ? 'primary' : 'neutral'" @click="toggleTag(tag)" :label="$t(tag)" />
         </div>
         <ul v-if="selectedTags.length > 0" class="flex flex-wrap gap-6 mt-6">
             <template v-for="satzmodell in satzmodelle" :key="satzmodell">
@@ -34,3 +46,4 @@ function toggleTag(tag) {
         </ul>
     </div>
 </template>
+

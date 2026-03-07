e0e287819f38adabf8f72af78a473095c64a304d
percussionfloh <florian.moldaschl@web.de>
2026-03-07 23:52:10 +0100

    update path.vue

--- app/pages/satzmodelle/[...path].vue
+++ app/pages/satzmodelle/[...path].vue
@@ -26,8 +26,12 @@ const { localScoreUrlGenerator } = useScoreUrlGenerator();
         <h1 class="font-bold text-3xl">{{page.title}}</h1>
         <VerovioCanvas :url="localScoreUrlGenerator(page.scorePath)" class="bg-white"/>
         <div class="flex gap-2">
-            <UBadge class="flex gap-4" size="lg" v-for="tag in page.tags" :key="tag">{{ $t(tag) }}</UBadge>
+            <div v-for="tag in page.tags" :key="tag">
+                <UBadge size="lg">{{ $t(tag) }}</UBadge>
+            </div>
         </div>
         <ContentRenderer v-if="page" :value="page" />
     </div>
 </template>
+
+                

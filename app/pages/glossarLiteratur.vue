c9623a5441695535480978b8c10fa9da729dd577
percussionfloh <florian.moldaschl@web.de>
2026-03-24 17:19:28 +0100

    update Literatur

--- app/pages/glossarLiteratur.vue
+++ app/pages/glossarLiteratur.vue
@@ -38,18 +38,19 @@ const localePath = useLocalePath();
             </section>
             <section>
                 <Heading class="font-bold text-4xl">Literatur </Heading>
-                <div class="flex flex-col gap-12 my-12">
-                    <div>
-                        <p class="text-lg">
-                            Drescher, Wolfgang (2019): "Satzmodelle und Harmonische Wendungen"
-                        </p>
-                        <p class="text-lg">
-                            Omnibus (Musik) (03/2026):  https://de.wikipedia.org/wiki/Omnibus_(Musik)
-                        </p>
-                        <p class="text-lg">
-                            Dittrich, Marie-Agnes (2007), »›Teufelsmühle‹ und ›Omnibus‹«, Zeitschrift der Gesellschaft für Musiktheorie 4/1–2, 107–121. https://doi.org/10.31751/247
-                        </p>
-                    </div>
+                <div class="flex flex-col gap-7 my-12">
+                    <p class="text-lg">
+                        Drescher, Wolfgang (2019): "Satzmodelle und Harmonische Wendungen"
+                    </p>
+                    <p class="text-lg">
+                        Omnibus (Musik) (03/2026):  https://de.wikipedia.org/wiki/Omnibus_(Musik)
+                    </p>
+                    <p class="text-lg">
+                        Dittrich, Marie-Agnes (2007), »›Teufelsmühle‹ und ›Omnibus‹«, Zeitschrift der Gesellschaft für Musiktheorie 4/1–2, 107–121. https://doi.org/10.31751/247
+                    </p>
+                    <p class="text-lg">
+                        openmusic.academy, Artikel »Zirkelmodelle (1) - Das ›Wegweiser‹-Modell«, https://openmusic.academy/docs/o4GKsjbXs6UDD9vqmLPSLf/musikhochschule-satztechnik-zirkelmodelle-wegweiser, 24.03.2026, 17:10
+                    </p>
                 </div>
             </section>
         </SectionContainer>
